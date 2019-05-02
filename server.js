18c97e294b0e2b672afc0b1e278e9f46a39a6d47
OKOJ <aajmd@hotmail.com>
2019-05-02 16:48:29 -0600

    routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var path = require("path");
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,10 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "public/home.html"));
-});
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
