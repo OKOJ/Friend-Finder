7b61b353270d23470e026de63bda7efb5a333664
OKOJ <aajmd@hotmail.com>
2019-05-02 16:51:30 -0600

    routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,8 @@
 var path = require("path");
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+app.get("/survey", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
